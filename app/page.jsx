8ab6bd4dfc7f81ecaff1d26d251c022d8c1b6b72
ASSLAN7 <aslann.3@icloud.com>
2025-09-13 16:04:14 +0200

    Delete app directory

--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{maxWidth:900, margin:'0 auto', padding:24}}>
-      <h1>LPP invest</h1>
-      <p>Minimal-Startseite ✔</p>
-    </main>
-  )
-}
